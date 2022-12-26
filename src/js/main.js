06da364235ca20c3c96528d297bff7918f07a551
Sarvack <sarvajanaprya@gmail.com>
2022-12-26 16:24:32 +0800

    new

--- src/js/main.js
+++ src/js/main.js
@@ -13,22 +13,83 @@ Date.prototype.addDays = function (days) {
     return date;
 }
 
+//times
+
+window.addEventListener("load", () => {
+    clock();
+    function clock() {
+      const today = new Date();
+  
+      // get time components
+      const hours = today.getHours();
+      const minutes = today.getMinutes();
+      const seconds = today.getSeconds();
+  
+      //add '0' to hour, minute & second when they are less 10
+      const hour = hours < 10 ? "0" + hours : hours;
+      const minute = minutes < 10 ? "0" + minutes : minutes;
+      const second = seconds < 10 ? "0" + seconds : seconds;
+  
+      //make clock a 12-hour time clock
+      const hourTime = hour > 12 ? hour - 12 : hour;
+  
+      // if (hour === 0) {
+      //   hour = 12;
+      // }
+      //assigning 'am' or 'pm' to indicate time of the day
+      const ampm = hour < 12 ? "AM" : "PM";
+  
+      // get date components
+      const month = today.getMonth();
+      const year = today.getFullYear();
+      const day = today.getDate();
+  
+      //declaring a list of all months in  a year
+      const monthList = [
+        "January",
+        "February",
+        "March",
+        "April",
+        "May",
+        "June",
+        "July",
+        "August",
+        "September",
+        "October",
+        "November",
+        "December"
+      ];
+  
+      //get current date and time
+      const date = monthList[month] + " " + day + ", " + year;
+      const time = hourTime + ":" + minute + ":" + second + ampm;
+  
+      //combine current date and time
+      const dateTime = date + " - " + time;
+  
+      //print current date and time to the DOM
+      document.getElementById("date-time").innerHTML = dateTime;
+      setTimeout(clock, 1000);
+    }
+  });
+
+  
 // Global variable
 const title_name = "Ade Sarva";
 
 const menu_list = [
-    {
-        title: "Search",
-        icon: "bi bi-search"
-    },
-    {
-        title: "Gallery",
-        icon: "bi-columns-gap"
-    },
-    {
-        title: "Schedule",
-        icon: "bi-calendar4-week"
-    }
+    // {
+    //     title: "Search",
+    //     icon: "bi bi-search"
+    // },
+    // {
+    //     title: "brichsaint",
+    //     icon: "bi-columns-gap"
+    // },
+    // {
+    //     title: "Schedule",
+    //     icon: "bi-calendar4-week"
+    // }
 ];
 
 const gallery_list = [
